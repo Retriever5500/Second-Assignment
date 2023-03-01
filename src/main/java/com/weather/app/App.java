e2d00fac915f81903a0acd91f23fd17bdafb2935
Retriever5500 <Retriever5500@gmail.com>
2023-03-01 10:22:48 +0330

    Swutching to maven

---
+++ src/main/java/com/weather/app/App.java
@@ -0,0 +1,55 @@
+package com.weather.app;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+import java.util.Scanner;
+
+public class App {
+    // Copy your API-KEY here
+    public final static String apiKey = "API-KEY";
+    // TODO: Write main function
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Retrieves weather data for the specified city.
+     *
+     * @param city the name of the city for which weather data should be retrieved
+     * @return a string representation of the weather data, or null if an error occurred
+     */
+    public static String getWeatherData(String city) {
+        try {
+            URL url = new URL("http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + city);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            reader.close();
+            return stringBuilder.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // TODO: Write getTemperature function returns celsius temperature of city by given json string
+    public static double getTemperature(String weatherJson){
+        double answer = 0.0;
+        return answer;
+    }
+
+    // TODO: Write getHumidity function returns humidity percentage of city by given json string
+    public static int getHumidity(String weatherJson){
+        int answer = 0;
+        return answer;
+    }
+}
+
